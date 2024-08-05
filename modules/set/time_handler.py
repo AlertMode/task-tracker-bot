d6b678f165c226117db053f71ec6b430cfdef912
Oleksii_Borovykov <aleksaybor@gmail.com>
2024-08-05 21:25:17 +0300

    Trying to fix the message deletion issue. The next step is to find something better than using state's storage in order write there message_id and then delete it. I'm thinking about using a separate file for this purpose. This way I can avoid the issue with the state's storage. Still have no clue what it will be excatly.

--- modules/set/time_handler.py
+++ modules/set/time_handler.py
@@ -9,7 +9,6 @@ from utils.dictionary import *
 
 router = Router(name=__name__)
 
-
 @router.message(
         CreateState.reminder_time,
         F.text.cast(validate_time_format).as_("time")
@@ -32,21 +31,16 @@ async def handle_reminder_time_input(
     Returns:
         None
     """
-    #TODO: Figure out how to delete the instrucion messages.
+    #TODO: Still find the the way to delete the messagesm, but withoug using states
     try:
+        await message.delete()
         await state.update_data(reminder_time=time)
         await state.set_state(CreateState.reminder_interval)
-        await store_message_id(
-            state=state,
-            message_id=message.message_id
-        )
         await bot.send_message(
             chat_id=message.from_user.id,
             text=task_reminder_interval_selection,
             reply_markup=reminder_interval_selection_kb()
         )
-        await message.delete()
-
         print(f'Time: {time}')
     except Exception as error:
         logger.error(f"handle_reminder_time_input: {error}")
@@ -57,7 +51,6 @@ async def handle_reminder_time_input(
 )
 async def handle_reminder_invalid_time_input(
     message: Message,
-    state: FSMContext,
     bot: Bot
 ) -> None:
     """
@@ -72,12 +65,12 @@ async def handle_reminder_invalid_time_input(
         None
     """
     try:
+        await message.delete()
         await bot.send_message(
             chat_id=message.from_user.id,
             text=task_remimder_invalid_time_format + '\n' + 
-                task_reminder_time,
+            task_reminder_time,
             reply_markup=None
         )
-        await message.delete()
     except Exception as error:
         logger.error(f"handle_reminder_invalid_time_input: {error}")
