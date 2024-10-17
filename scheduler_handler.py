2bdc71ef96883801f53595bcda7a9ae165d4ec07
Oleksii_Borovykov <aleksaybor@gmail.com>
2024-10-17 21:10:29 +0300

    Finished implementing the scheduler handler. It is now able to schedule tasks and execute them. This is the first working prototype of the scheduler handler.

--- scheduler_handler.py
+++ scheduler_handler.py
@@ -4,17 +4,44 @@ import asyncio
 
 from aiogram import Bot
 import aioschedule as shcedule
+from sqlalchemy import Sequence
 
-from bot_instance import TOKEN
-from database.database import DataBase
+from database.database import *
+from modules.list.task_list_callback import *
+from modules.alter.task_alteration_kb import *
+from utils.dictionary import *
 from utils.logging_config import logger
 
 
-bot = Bot(token=TOKEN)
-db = DataBase()
+async def send_reminder(bot: Bot, task: Tasks, user_id: str) -> None:
+    """
+    Sends a reminder to the user about the task.
+    Allows the user to mark the task as done or delete the task.
+    
+    Args:
+        bot (Bot): The bot instance.
+        task (Task): The task to remind the user about.
+
+    Returns:
+        None
+    """
+    try:
+        await bot.send_message(
+            chat_id=user_id,
+            text = (
+                task_ongoing % (
+                    task.creation_date,
+                    task.description,
+                    task.reminder_date
+                )),
+            reply_markup=task_ongoing_kb(task.id)
+        )
+      
+    except Exception as error:
+        logger.error(f"send_reminder: {error}")
 
 
-async def check_reminders(bot: Bot, db: DataBase) -> None:
+async def check_reminders(bot: Bot, database: DataBase) -> None:
     """
     Checks the reminders and sends the reminders to the users.
     
@@ -26,11 +53,40 @@ async def check_reminders(bot: Bot, db: DataBase) -> None:
         None
     """
     try:
-        #TODO: Finish the implementation.
-        pass
+        start_of_minute = datetime.now().replace(second=0, microsecond=0)
+        users = await database.get_all_users()
+
+        for user in users:
+            tasks = await database.get_all_tasks_by_user(
+                user_id=user.id,
+                status=TaskStatus.ONGOING
+            )
+
+            for task in tasks:
+                if task.reminder_date and task.reminder_date <= start_of_minute:
+                    await send_reminder(bot=bot, task=task, user_id=user.telegram_id)
+            #TODO: Set the reminder_date to None after sending the reminder
+            # or create a separate column for the reminder_date
+
     except Exception as error:
         logger.error(f"check_reminders: {error}")
         
-    
 
-        
+async def run_scheduler(bot: Bot, database: DataBase) -> None:
+    """
+    Runs the scheduler to check the reminders.
+    
+    Args:
+        bot (Bot): The bot instance.
+        database (DataBase): The database instance.
+    
+    Returns:
+        None
+    """
+    try:
+        while True:
+            await check_reminders(bot=bot, database=database)
+            await asyncio.sleep(60)
+    
+    except Exception as error:
+        logger.error(f"run_scheduler: {error}")
